a914d09857aea77df6bf1abe83fe68ad46faf7ab
Nightfurya <y.m.logvinenko@gmail.com>
2019-08-14 22:15:19 +0300

    reorganized project structure; login screen added

---
+++ src/Components/EnterPopUp/EnterPopUp.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./EnterPopUp.css";
+
+class EnterPopUp extends Component {
+  state = {
+    hidden: false
+  };
+
+  hidePopup = () => {
+    this.setState({
+      hidden: !this.state.hidden
+    });
+  };
+
+  render() {
+    return (
+      <div className="popup-wrapper" style={{ display: this.state.hidden ? "none" : "flex" }}>
+        <div className="title">Do you wanna play?</div>
+        <button className="accept-btn" onClick={this.hidePopup}>
+          YES!
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EnterPopUp;
---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Game.css";
+import EnterPopUp from "./Components/EnterPopUp/EnterPopUp";
+
+class Game extends Component {
+  render() {
+    return <EnterPopUp />;
+  }
+}
+
+export default Game;
