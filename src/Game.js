391ea13bac14dabba310350cd069dc9327f52db9
Nightfurya <y.m.logvinenko@gmail.com>
2019-08-15 19:36:43 +0300

    board added; 1st stage game process finished

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import "./Game.css";
 import EnterPopUp from "./Components/EnterPopUp/EnterPopUp";
 import Header from "./Components/Header/Header";
+import Board from "./Components/Board/Board";
 
 class Game extends Component {
   render() {
     return (
       <>
-        <EnterPopUp />;
-        <Header />;
+        <EnterPopUp />
+        <Header />
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+        </div>
       </>
     );
   }
