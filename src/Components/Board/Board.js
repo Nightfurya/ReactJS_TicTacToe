7494c7dd5d4337678d229e8713aded26c1f4823a
Nightfurya <y.m.logvinenko@gmail.com>
2019-08-16 19:44:46 +0300

    Showing previous moves

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -3,51 +3,13 @@ import "./Board.css";
 import Square from "../Square/Square";
 
 class Board extends Component {
-  state = {
-    squares: Array(9).fill(null),
-    xIsNext: true
-  };
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
-
-  calculateWinner = filledSquares => {
-    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    const linesLength = lines.length;
-    for (let i = 0; i < linesLength; i++) {
-      const [a, b, c] = lines[i];
-      if (filledSquares[a] && filledSquares[a] === filledSquares[b] && filledSquares[a] === filledSquares[c]) {
-        return filledSquares[a];
-      }
-    }
-    return null;
-  };
 
   render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = "Победитель: " + winner;
-    } else {
-      status = "Следующий игрок: " + (this.state.xIsNext ? "X" : "O");
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/Game.js
+++ src/Game.js
@@ -5,14 +5,85 @@ import Header from "./Components/Header/Header";
 import Board from "./Components/Board/Board";
 
 class Game extends Component {
+  state = {
+    history: [{ squares: Array(9).fill(null) }],
+    isXNext: true,
+    stepNumber: 0
+  };
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const filledSquares = current.squares.slice();
+
+    if (this.calculateWinner(filledSquares) || filledSquares[i]) {
+      return;
+    }
+    filledSquares[i] = this.state.isXNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: filledSquares
+        }
+      ]),
+      isXNext: !this.state.isXNext,
+      stepNumber: history.length
+    });
+  }
+
+  calculateWinner = filledSquares => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    const linesLength = lines.length;
+    for (let i = 0; i < linesLength; i++) {
+      const [a, b, c] = lines[i];
+      if (filledSquares[a] && filledSquares[a] === filledSquares[b] && filledSquares[a] === filledSquares[c]) {
+        return filledSquares[a];
+      }
+    }
+    return null;
+  };
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      isXNext: step % 2 === 0
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move № " + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button className="move-to-btn" onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Победитель: " + winner;
+    } else {
+      status = "Следующий игрок: " + (this.state.isXNext ? "X" : "O");
+    }
+
     return (
       <>
         <EnterPopUp />
         <Header />
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          </div>
+          <div className="game-info">
+            <div className="game-info-winner">{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       </>
